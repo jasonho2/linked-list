4b7b990a65e18c576c6c90f475162e6e717a5555
jasonho2 <31460284+jasonho2@users.noreply.github.com>
2025-01-30 15:46:00 -0800

    Uncomment puts lines for testing

--- main.rb
+++ main.rb
@@ -4,10 +4,10 @@ list = LinkedList.new
 
 list.append('dog')
 list.append('cat')
-#list.append('parrot')
-#list.append('hamster')
-#list.append('snake')
-#list.append('turtle')
+list.append('parrot')
+list.append('hamster')
+list.append('snake')
+list.append('turtle')
 
 puts list.to_s
 puts list.size
