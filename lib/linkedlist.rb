3d37a77b3ec78f0fed3c6ef76427d273889df253
jasonho2 <31460284+jasonho2@users.noreply.github.com>
2025-01-29 20:51:22 -0800

    Append and Prepend methods complete

---
+++ lib/linkedlist.rb
@@ -0,0 +1,51 @@
+class Node
+
+  attr_accessor :node, :next_node
+
+  def initialize(value)
+    @node = value
+    @next_node = nil
+  end
+
+end
+
+class LinkedList
+
+  attr_accessor :head, :tail
+
+  def initialize
+    @head = nil
+  end
+  
+  # append method
+  def append(value)
+    new_node = Node.new(value)
+
+    if @head.nil?
+      @head = new_node
+    else
+      current = @head
+      while current.next_node != nil
+        current = current.next_node
+      end
+      current.next_node = new_node
+    end
+
+  end
+
+  # prepend method
+  def prepend(value)
+    new_node = Node.new(value)
+
+    if @head.nil?
+      @head = new_node
+    else
+      new_node.next_node = @head
+      @head = new_node
+    end
+
+  end
+
+  
+
+end
---
+++ main.rb
@@ -0,0 +1,12 @@
+require_relative 'lib/linkedlist.rb'
+
+list = LinkedList.new
+
+list.append('dog')
+list.append('cat')
+list.append('parrot')
+list.append('hamster')
+list.append('snake')
+list.prepend('turtle')
+
+puts list.inspect
